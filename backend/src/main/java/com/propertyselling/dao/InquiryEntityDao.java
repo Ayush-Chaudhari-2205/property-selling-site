db54784142d7070b42edfbaa58f4edf797ee3202
Piyush Patil <piyushpatil0310@gmail.com>
2025-02-09 17:59:37 +0530

    Added WishlistService

--- backend/src/main/java/com/propertyselling/dao/InquiryEntityDao.java
+++ backend/src/main/java/com/propertyselling/dao/InquiryEntityDao.java
@@ -5,4 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface InquiryEntityDao extends JpaRepository<Inquiry, Long> {
+    List<Inquiry> findByPropertyId(Long propertyId);
+
+    List<Inquiry> findByBuyerId(Long buyerId);
+
+    @Query("SELECT i FROM Inquiry i WHERE i.property.seller.id = :sellerId")
+    List<Inquiry> findByPropertySellerId(@Param("sellerId") Long sellerId);
 }
